b67d3ee97ec34398b6712d248db34749e139e468
olewk <olewkere@gmail.com>
2023-10-02 14:48:16 +0300

    Lb2

---
+++ LbS/lb_2/lb_2_Temchenko.py
@@ -0,0 +1,26 @@
+print("Programming", "Essential", "in")
+print("Python\n")
+
+def print_arrow():
+    arrow= [
+        "    *",
+        "   * *",
+        "  *   *",
+        " *     *",
+        "***   ***",
+        "  *   *",
+        "  *   *",
+        "  *   *",
+        "  *****\n",
+    ]
+    for line in arrow:
+        print(line)
+print_arrow()
+
+print("I'm student")
+
+print("\nI'm \nlearning\nPython!")
+
+number1 = 0o500
+number2 = 0x777
+print("\nnumber 1 = ",number1," number 2 = ",number2)
