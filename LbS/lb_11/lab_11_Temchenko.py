bd64a80983f00711f4e1bd7d878d186d9ee64bb2
olewk <olewkere@gmail.com>
2023-11-15 10:59:33 +0200

    Lb11

--- LbS/lb_11/lab_11_Temchenko.py
+++ LbS/lb_11/lab_11_Temchenko.py
@@ -4,19 +4,15 @@ class QueueError(Exception):
 class Queue:
     def __init__(self):
         self.items = []
-
     def is_empty(self):
         return len(self.items) == 0
-
     def put(self, item):
         self.items.append(item)
-
     def get(self):
         if not self.is_empty():
             return self.items.pop(0)
         else:
             raise QueueError("Queue is empty")
-
 class SuperQueue(Queue):
     def isempty(self):
         return self.is_empty()
