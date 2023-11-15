114c572d3db81a252bc4537d7fb9c7742af38566
olewk <olewkere@gmail.com>
2023-11-15 10:57:34 +0200

    Lb10

---
+++ LbS/lb_11/lab_11_Temchenko.py
@@ -0,0 +1,32 @@
+class QueueError(Exception):
+    pass
+
+class Queue:
+    def __init__(self):
+        self.items = []
+
+    def is_empty(self):
+        return len(self.items) == 0
+
+    def put(self, item):
+        self.items.append(item)
+
+    def get(self):
+        if not self.is_empty():
+            return self.items.pop(0)
+        else:
+            raise QueueError("Queue is empty")
+
+class SuperQueue(Queue):
+    def isempty(self):
+        return self.is_empty()
+
+que = SuperQueue()
+que.put(1)
+que.put("dog")
+que.put(False)
+for i in range(4):
+    if not que.isempty():
+        print(que.get())
+    else:
+        print("Queue empty")
